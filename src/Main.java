5af4af8d7c42efd9e9bea14c4f7ab316347c06e2
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-26 17:40:31 -0400

    Populate instances of Card and Deck

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
         Card card_1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
         Card card_2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
         Card card_3 = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM");
+
+        ArrayList<Card> cards = new ArrayList<Card>();
+        cards.add(card_1);
+        cards.add(card_2);
+        cards.add(card_3);
+
+        Deck deck = new Deck(cards);
 
         System.out.println(card_1.getQuestion());
         System.out.println(card_1.getAnswer());
@@ -19,5 +27,6 @@ public class Main {
         System.out.println(turn_1.isCorrectGuess());
 
         System.out.println(turn_1.guessFeedback());
+        System.out.println(deck.getCards());
     }
 }
