53194d426206c04140180ebf2c778588958537aa
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-26 17:10:24 -0400

    Adjust spacing

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
         Card card_1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
         Card card_2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
         Card card_3 = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM");
-        
 
         System.out.println(card_1.getQuestion());
         System.out.println(card_1.getAnswer());
