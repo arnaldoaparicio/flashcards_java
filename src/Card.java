79aa2b59938248018e9e5033a45c2f329b7788cb
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-26 15:51:18 -0400

    Add 'question', 'answer', and 'category' variables

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,26 @@
 public class Card {
+
+    private String question;
+    private String answer;
+    private String category;
+
+
+    Card(String question, String answer, String category) {
+        this.question = question;
+        this.answer = answer;
+        this.category = category;
+    }
+
+        public String getQuestion() {
+            return this.question;
+        }
+
+        public String getAnswer() {
+            return this.answer;
+        }
+
+        public String getCategory() {
+            return this.category;
+        }
+    }
 }
