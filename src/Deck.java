fe21dfc4b1d9aced3bf22f1cfb82d97349e37558
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-26 17:41:32 -0400

    Add 'getCards' method

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Deck {
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> cards = new ArrayList<Card>();
+
+    Deck(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+
+    public String getCards() {
+        return this.cards.toString();
+    }
 }
