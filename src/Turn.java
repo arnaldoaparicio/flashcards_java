b55fb40c74fa15c00d36dc16a642cdc0e1cd37da
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-26 16:15:34 -0400

    Add card variable

--- src/Turn.java
+++ src/Turn.java
@@ -1,9 +1,11 @@
 public class Turn {
 
     private String guess;
+    private Card card;
 
-    Turn(String guess) {
+    Turn(String guess, Card card) {
         this.guess = guess;
+        this.card = card;
 
     }
 }
