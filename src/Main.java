24ca4d0714382738197fa7d4da097411086430ca
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-26 17:08:34 -0400

    Add more instances of Card

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Card card_1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
+        Card card_2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
+        Card card_3 = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM");
+        
 
         System.out.println(card_1.getQuestion());
         System.out.println(card_1.getAnswer());
