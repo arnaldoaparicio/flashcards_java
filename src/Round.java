5b7e363f82f2a43ba598ebdc64f679045e8224a8
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-27 20:09:41 -0400

    Add 'getDeck', 'getTurnsTaken', and 'currentCard' methods

--- src/Round.java
+++ src/Round.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
 public class Round {
+
+    private Deck deck;
+    private ArrayList<Turn> turnsTaken = new ArrayList<Turn>();
+
+    Round(Deck deck) {
+        this.deck = deck;
+    }
+
+    public String getDeck() {
+        return deck.toString();
+    }
+    
+    public String getTurnsTaken() {
+        return turnsTaken.toString();
+    }
+
+    public String currentCard() {
+        return deck.allCards().get(0).toString();
+    }
 }
