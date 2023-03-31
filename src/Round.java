dc13277f718f95ec4631420583d771e113adb71a
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-31 12:42:27 -0400

    Modify 'getTurnsTaken' method to print out card details

--- src/Round.java
+++ src/Round.java
@@ -13,7 +13,13 @@ public class Round {
     }
     
     public String getTurnsTaken() {
-        return turnsTaken.toString();
+        ArrayList<String> formattedTurnsTaken = new ArrayList<String>();
+        
+        turnsTaken.forEach(turn -> {
+            formattedTurnsTaken.add(String.format("%s %s question= %s, answer= %s, category= %s", 
+            turn.toString(), turn.getCard(), turn.getCard().getQuestion(), turn.getCard().getAnswer(), turn.getCard().getCategory()));
+        });
+        return formattedTurnsTaken.toString();
     }
 
     public String currentCard() {
