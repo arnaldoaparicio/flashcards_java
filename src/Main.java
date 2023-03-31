23ed34399490f41e9d158731449bdb498ea80dd4
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-31 17:18:44 -0400

    Omit all code

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,24 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Card card_1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
-        Card card_2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
-        Card card_3 = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM");
+        // Card card_1 = new Card("What is the capital of Alaska?", "Juneau", "Geography");
+        // Card card_2 = new Card("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", "STEM");
+        // Card card_3 = new Card("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", "STEM");
 
-        ArrayList<Card> cards = new ArrayList<Card>();
-        cards.add(card_1);
-        cards.add(card_2);
-        cards.add(card_3);
+        // ArrayList<Card> cards = new ArrayList<Card>();
+        // cards.add(card_1);
+        // cards.add(card_2);
+        // cards.add(card_3);
 
-        Deck deck = new Deck(cards);
+        // Deck deck = new Deck(cards);
+        // Turn turn_1 = new Turn("Juneau", card_1);
+        
+        // Round round_1 = new Round(deck);
 
 //        System.out.println(card_1.getQuestion());
 //        System.out.println(card_1.getAnswer());
 //        System.out.println(card_1.getCategory());
 
-        Turn turn_1 = new Turn("Juneau", card_1);
-
-        Round round_1 = new Round(deck);
 
 //        System.out.println(turn_1.getGuess());
 //        System.out.println(turn_1.getCard());
@@ -36,19 +36,19 @@ public class Main {
 
         // System.out.println(round_1.getDeck());
         // System.out.println(round_1.getTurnsTaken());
-        System.out.println("Current card: " + round_1.currentCard());
-        System.out.println("Number of turns: " + round_1.turnsCount());
-        System.out.println("Number correct: " + round_1.numberCorrect());
-        round_1.takeTurn(turn_1.getGuess());
-        System.out.println("All turns taken: " + round_1.getTurnsTaken());
-        System.out.println("All cards: " + deck.formattedAllCards());
-        System.out.println("Current card: " + round_1.currentCard());
-        System.out.println("Number of turns: " + round_1.turnsCount());
-        System.out.println("Latest card feedback: " + round_1.latestCardFeedback());
-        System.out.println("Number correct: " + round_1.numberCorrect());
-        System.out.println("Percent correct: " + round_1.percentCorrect());
-        System.out.println("Number correct by category: " + round_1.numberCorrectByCategory("Geography"));
-        System.out.println("Percent correct by category: " + round_1.percentCorrectByCategory("Geography"));
+        // System.out.println("Current card: " + round_1.currentCard());
+        // System.out.println("Number of turns: " + round_1.turnsCount());
+        // System.out.println("Number correct: " + round_1.numberCorrect());
+        // round_1.takeTurn(turn_1.getGuess());
+        // System.out.println("All turns taken: " + round_1.getTurnsTaken());
+        // System.out.println("All cards: " + deck.formattedAllCards());
+        // System.out.println("Current card: " + round_1.currentCard());
+        // System.out.println("Number of turns: " + round_1.turnsCount());
+        // System.out.println("Latest card feedback: " + round_1.latestCardFeedback());
+        // System.out.println("Number correct: " + round_1.numberCorrect());
+        // System.out.println("Percent correct: " + round_1.percentCorrect());
+        // System.out.println("Number correct by category: " + round_1.numberCorrectByCategory("Geography"));
+        // System.out.println("Percent correct by category: " + round_1.percentCorrectByCategory("Geography"));
 
     }
 }
