a6e2b79134e136cbb676950479d02fcdbf480bf9
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-03 12:34:13 -0400

    Change 'percentCorrectByCategory' method from float type to integer type

--- src/Round.java
+++ src/Round.java
@@ -69,7 +69,7 @@ public class Round {
         return correctTurns.size();
     }
 
-    public Float percentCorrectByCategory(String category) {
+    public Integer percentCorrectByCategory(String category) {
         List<Turn> correctCategory = new ArrayList<Turn>();
         List<Turn> entireCategory = new ArrayList<Turn>();
 
@@ -86,6 +86,9 @@ public class Round {
         float correctCategoryFloat = (float) correctCategory.size();
         float entireCategoryFloat = (float) entireCategory.size();
 
-        return (correctCategoryFloat / entireCategoryFloat) * 100;
+        float result = (float) (correctCategoryFloat / entireCategoryFloat) * 100;
+        int resultInt = Math.round(result);
+       
+        return resultInt;
     }   
 }
