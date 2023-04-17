18b787a0a9c512d71d485e1acc7a4b5c52d4c090
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-17 19:05:47 -0400

    Edit card details

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Card card_1 = new Card("What is 5 + 5?", "10", "STEM");
+        Card card_1 = new Card("What is the first name of the main character in Silent Hill 3?", "heather", "Video Games");
         Card card_2 = new Card("What is Mike's middle name?", "nobody knows", "Turing Staff");
         Card card_3 = new Card("What is Rachel's favorite animal?", "panda", "Turing Staff");
         Card card_4 = new Card("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture");
@@ -36,7 +36,7 @@ public class Main {
         
         System.out.println("****** Game Over! ******");
         System.out.println("You have " + round.numberCorrect() + " out of " + round.turnsCount());
-        System.out.println("STEM - " + round.percentCorrectByCategory("STEM") + "% correct " + "(" + round.numberCorrectByCategory("STEM") + " correct)");
+        System.out.println("Video Games - " + round.percentCorrectByCategory("Video Games") + "% correct " + "(" + round.numberCorrectByCategory("Video Games") + " correct)");
         System.out.println("Turing Staff - " + round.percentCorrectByCategory("Turing Staff") + "% correct " + "("
                 + round.numberCorrectByCategory("Turing Staff") + " correct)");
         System.out.println("Pop Culture - " + round.percentCorrectByCategory("Pop Culture") + "% correct " + "("
