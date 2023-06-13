5127f7f4c8386335fbd435dc5dfaf7407ace2e53
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-13 15:16:23 -0400

    Add test to check for Card getters

--- src/CardTest.java
+++ src/CardTest.java
@@ -1,7 +1,19 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.api.Test;
+
 
 public class CardTest {
 
-  
+  private final Card card = new Card("What is my name?", "John Doe", "Names");
+
+  @Test
+  void groupedAssertions() {
+    assertAll("card",
+      () -> assertEquals("What is my name?", card.getQuestion()),
+      () -> assertEquals("John Doe", card.getAnswer()),
+      () -> assertEquals("Names", card.getCategory())
+      );
+  }
   
 }
