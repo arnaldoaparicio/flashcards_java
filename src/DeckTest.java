fa850f216d9ed9b2e81416afb6c911ebddae0a04
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-06-13 15:48:48 -0400

    Add test to check for first Card in Deck

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -1,3 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 public class DeckTest {
-  
+        private final Card card_1 = new Card("What is the first name of the main character in Silent Hill 3?", "heather", "Video Games");
+        private final Card card_2 = new Card("What is Mike's middle name?", "nobody knows", "Turing Staff");
+        private final Card card_3 = new Card("What is Rachel's favorite animal?", "panda", "Turing Staff");
+
+        private final ArrayList<Card> cards = new ArrayList<Card>();
+
+        private final Deck deck = new Deck(cards);
+
+        @Test
+        void checkFirstCardInDeck() {
+          cards.add(card_1);
+          cards.add(card_2);
+          cards.add(card_3);
+          
+          assertAll("deck",
+          () -> assertEquals("What is the first name of the main character in Silent Hill 3?", deck.firstCard().getQuestion()),
+          () -> assertEquals("heather", deck.firstCard().getAnswer()),
+          () -> assertEquals("Video Games", deck.firstCard().getCategory())
+          );
+        }
 }
